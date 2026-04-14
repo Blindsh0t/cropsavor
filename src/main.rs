fc407669784b18477acd8b365d63b312e883a9a9
Han Patel <hardik1236@gmail.com>
2026-04-14 15:40:00 -0700

    image: open the wallpaper and print its size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("circular image cropper");
+use image::GenericImageView;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_path = "/Users/han/Desktop/wallp-1.jpg";
+
+    let image = image::open(input_path)?;
+    let (width, height) = image.dimensions();
+
+    println!("loaded {width}x{height}");
+
+    Ok(())
 }
